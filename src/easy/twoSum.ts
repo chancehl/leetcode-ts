28c07c37998645554227515ca60f9bd4ca9fd792
Chance Linz <clinz.dev@gmail.com>
2023-04-05 13:37:58 -0700

    🐛 fix(twoSum.ts): optimize twoSum function by using a hash map to store values

--- src/easy/twoSum.ts
+++ src/easy/twoSum.ts
@@ -1,9 +1,13 @@
 function twoSum(nums: number[], target: number): number[] {
+    const map: Record<number, number> = {}
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+        const diff = target - nums[i]
+
+        if (map[diff] != null) {
+            return [map[diff], i]
+        } else {
+            map[nums[i]] = i
         }
     }
 

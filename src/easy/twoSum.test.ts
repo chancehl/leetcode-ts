ac1ef63c0ff26e463f0e4b955055b42155f3c485
Chance Linz <clinz.dev@gmail.com>
2023-04-05 13:23:54 -0700

    🚀 feat(twoSum.ts): add function to return indices of two numbers that add up to target 🔥 chore(index.ts): remove unused sum function 🔥 chore(index.test.ts): remove test for unused sum function

---
+++ src/easy/twoSum.test.ts
@@ -0,0 +1,15 @@
+import { twoSum } from './twoSum'
+
+describe('twoSum', () => {
+    test('TEST_CASE_1', () => {
+        expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+    })
+
+    test('TEST_CASE_2', () => {
+        expect(twoSum([3, 2, 4], 6)).toEqual([1, 2])
+    })
+
+    test('TEST_CASE_3', () => {
+        expect(twoSum([3, 3], 6)).toEqual([0, 1])
+    })
+})
--- src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from './index'
-
-describe('index', () => {
-    test('foo', () => {
-        expect(sum(2, 4)).toEqual(6)
-    })
-})

bfc2f9b3665a1f981058c9e82b9f21450014c012
Chance Linz <chanlinz@amazon.com>
2023-04-10 11:50:45 -0700

    separating test inputs in mergeTwoLists.spec.ts

--- src/easy/bestTimeToBuyAndSellStock/maxProfit.spec.ts
+++ src/easy/bestTimeToBuyAndSellStock/maxProfit.spec.ts
@@ -5,11 +5,11 @@ describe('maxProfit', () => {
 		expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(5)
 	})
 
-	// test('test case 2', () => {
-	// 	expect(maxProfit([7, 6, 4, 3, 2, 1])).toEqual(0)
-	// })
+	test('test case 2', () => {
+		expect(maxProfit([7, 6, 4, 3, 2, 1])).toEqual(0)
+	})
 
-	// test('test case 3', () => {
-	// 	expect(maxProfit([2, 4, 1])).toEqual(2)
-	// })
+	test('test case 3', () => {
+		expect(maxProfit([2, 4, 1])).toEqual(2)
+	})
 })
--- src/easy/mergeTwoSortedLists/mergeTwoLists.test.ts
+++ src/easy/mergeTwoSortedLists/mergeTwoLists.test.ts
@@ -4,7 +4,10 @@ import { mergeTwoLists } from './mergeTwoLists'
 
 describe('mergeTwoLists', () => {
     test('input 1', () => {
-        expect(mergeTwoLists(ListNode.from([1, 2, 4]), ListNode.from([1, 3, 4]))).toEqual(ListNode.from([1, 1, 2, 3, 4, 4]))
+        const listA = ListNode.from([1, 2, 4])
+        const listB = ListNode.from([1, 3, 4])
+
+        expect(mergeTwoLists(listA, listB)).toEqual(ListNode.from([1, 1, 2, 3, 4, 4]))
     })
 
     test('input 2', () => {

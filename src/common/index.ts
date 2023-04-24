7bdbf9c953a6d2f44f6424d8fc9bf26586da3afe
Chance Linz <chanlinz@amazon.com>
2023-04-24 13:07:16 -0700

    ✨ feat(common): add Queue data structure implementation ✅ test(queue): add tests for Queue data structure implementation

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,2 +1,3 @@
 export * from './stack'
 export * from './linkedList'
+export * from './queue'
---
+++ src/common/queue/index.ts
@@ -0,0 +1,29 @@
+class Queue<T> {
+	elements: T[]
+
+	constructor(values: T[] = []) {
+		this.elements = values
+	}
+
+	peek(): T {
+		return this.elements[this.elements.length - 1]
+	}
+
+	enqueue(value: T): void {
+		this.elements.push(value)
+	}
+
+	dequeue(): T | undefined {
+		return this.elements.shift()
+	}
+
+	toString(): string {
+		return this.elements.join(',')
+	}
+
+	getElements(): T[] {
+		return this.elements
+	}
+}
+
+export { Queue }

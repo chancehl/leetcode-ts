8e2f2e3a49768ace3f5e6146d7cce7af54f69774
Chance Linz <clinz.dev@gmail.com>
2023-04-24 19:08:43 -0700

    🎁 chore(package.json): add husky as a dev dependency and add prepare script to install husky 🔧 chore(pre-commit): add pre-commit hook to run format and test scripts before committing
    🔨 refactor(binaryTree/index.ts): fix indentation and remove unnecessary semicolons

--- src/common/binaryTree/index.ts
+++ src/common/binaryTree/index.ts
@@ -4,82 +4,81 @@ import { Stack } from '@common/stack'
 type Order = 'pre-order' | 'post-order' | 'in-order'
 
 class TreeNode {
-	val: number
-	left: TreeNode | null
-	right: TreeNode | null
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
 
-	private visited: boolean = false // note: this is exclusively used for traversal, searching
+    private visited: boolean = false // note: this is exclusively used for traversal, searching
 
-	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-		this.val = val ?? 0
-		this.left = left ?? null
-		this.right = right ?? null
-	}
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val ?? 0
+        this.left = left ?? null
+        this.right = right ?? null
+    }
 
-	traverse(traversal: 'bfs' | 'dfs', order: Order = 'pre-order'): number[] {
-		if (traversal == 'bfs') {
-			return this.breadthFirstTraversal(order)
-		} else {
-			return this.depthFirstTraversal(order)
-		}
-	}
+    traverse(traversal: 'bfs' | 'dfs', order: Order = 'pre-order'): number[] {
+        if (traversal == 'bfs') {
+            return this.breadthFirstTraversal(order)
+        } else {
+            return this.depthFirstTraversal(order)
+        }
+    }
 
-	private breadthFirstTraversal(_order: Order): number[] {
-		const queue = new Queue<TreeNode>([this])
+    private breadthFirstTraversal(_order: Order): number[] {
+        const queue = new Queue<TreeNode>([this])
 
-		let elements: number[] = []
+        let elements: number[] = []
 
-		let current: TreeNode | undefined;
+        let current: TreeNode | undefined
 
-		while (queue.elements.length > 0) {
-			current = queue.dequeue()
+        while (queue.elements.length > 0) {
+            current = queue.dequeue()
 
-			if (current) {
-				elements.push(current.val)
-			}
+            if (current) {
+                elements.push(current.val)
+            }
 
-			// enqueue left
-			if (current && current.left && current.left.visited === false) {
-				queue.enqueue(current.left)
-				current.left.visited = true
-			}
+            // enqueue left
+            if (current && current.left && current.left.visited === false) {
+                queue.enqueue(current.left)
+                current.left.visited = true
+            }
 
-			// enqueue right
-			if (current && current.right && current.right.visited === false) {
-				queue.enqueue(current.right)
-				current.right.visited = true
-			}
-		}
+            // enqueue right
+            if (current && current.right && current.right.visited === false) {
+                queue.enqueue(current.right)
+                current.right.visited = true
+            }
+        }
 
-		return elements
-	}
+        return elements
+    }
 
-	private depthFirstTraversal(_order: Order): number[] {
-		const stack = new Stack<TreeNode>([this])
+    private depthFirstTraversal(_order: Order): number[] {
+        const stack = new Stack<TreeNode>([this])
 
-		let elements: number[] = []
+        let elements: number[] = []
 
-		let current: TreeNode | undefined;
+        let current: TreeNode | undefined
 
-		while (stack.elements.length) {
-			current = stack.pop()
+        while (stack.elements.length) {
+            current = stack.pop()
 
-			if (current) {
-				elements.push(current.val)
-			}
+            if (current) {
+                elements.push(current.val)
+            }
 
-			if (current && current.right) {
-				stack.push(current.right)
-			}
+            if (current && current.right) {
+                stack.push(current.right)
+            }
 
-			if (current && current.left) {
-				stack.push(current.left)
-			}
+            if (current && current.left) {
+                stack.push(current.left)
+            }
+        }
 
-		}
-
-		return elements
-	}
+        return elements
+    }
 }
 
 export { TreeNode }

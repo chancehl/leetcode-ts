583edf8c2fd65d4ea643f2f290a18f951bc92290
Chance Linz <clinz.dev@gmail.com>
2023-04-24 18:35:24 -0700

    ✅ test(queue): add tests for queue.peek(), queue.enqueue(), and queue.dequeue() methods

--- src/common/queue/index.test.ts
+++ src/common/queue/index.test.ts
@@ -1,74 +1,73 @@
 import { Queue } from './index'
 
 describe('queue', () => {
-	describe('queue.peek()', () => {
-		test('returns a value if the queue length >= 1', () => {
-			const queue = new Queue([1, 2, 3, 4, 5])
+    describe('queue.peek()', () => {
+        test('returns a value if the queue length >= 1', () => {
+            const queue = new Queue([1, 2, 3, 4, 5])
 
-			expect(queue.peek()).toEqual(5)
-		})
+            expect(queue.peek()).toEqual(5)
+        })
 
-		test('returns undefined if the queue length === 0', () => {
-			const queue = new Queue()
+        test('returns undefined if the queue length === 0', () => {
+            const queue = new Queue()
 
-			expect(queue.peek()).toEqual(undefined)
-		})
-	})
+            expect(queue.peek()).toEqual(undefined)
+        })
+    })
 
-	describe('queue.enqueue()', () => {
-		test('can enqueue a number to an empty queue', () => {
-			const queue = new Queue()
+    describe('queue.enqueue()', () => {
+        test('can enqueue a number to an empty queue', () => {
+            const queue = new Queue()
 
-			queue.enqueue(1)
+            queue.enqueue(1)
 
-			expect(queue.getElements()).toEqual([1])
-		})
+            expect(queue.elements).toEqual([1])
+        })
 
-		test('can enqueue a number to an existing queue', () => {
-			const queue = new Queue([1, 2, 3])
+        test('can enqueue a number to an existing queue', () => {
+            const queue = new Queue([1, 2, 3])
 
-			queue.enqueue(4)
+            queue.enqueue(4)
 
-			expect(queue.getElements()).toEqual([1, 2, 3, 4])
-		})
+            expect(queue.elements).toEqual([1, 2, 3, 4])
+        })
 
-		test('can enqueue multiple elements', () => {
-			const queue = new Queue()
+        test('can enqueue multiple elements', () => {
+            const queue = new Queue()
 
-			queue.enqueue(1)
-			queue.enqueue(2)
-			queue.enqueue(3)
-			queue.enqueue(4)
+            queue.enqueue(1)
+            queue.enqueue(2)
+            queue.enqueue(3)
+            queue.enqueue(4)
 
-			expect(queue.getElements()).toEqual([1, 2, 3, 4])
-		})
-	})
+            expect(queue.elements).toEqual([1, 2, 3, 4])
+        })
+    })
 
-	describe('queue.dequeue()', () => {
-		test('returns undefined if queue length === 0', () => {
-			const queue = new Queue()
+    describe('queue.dequeue()', () => {
+        test('returns undefined if queue length === 0', () => {
+            const queue = new Queue()
 
-			expect(queue.dequeue()).toEqual(undefined)
-		})
+            expect(queue.dequeue()).toEqual(undefined)
+        })
 
-		test('returns a value if queue length >= 1', () => {
-			const queue = new Queue([1, 2, 3])
+        test('returns a value if queue length >= 1', () => {
+            const queue = new Queue([1, 2, 3])
 
-			expect(queue.dequeue()).toEqual(1)
-		})
+            expect(queue.dequeue()).toEqual(1)
+        })
 
+        test('can dequeue multiple elements', () => {
+            const queue = new Queue([1, 2, 3])
 
-		test('can dequeue multiple elements', () => {
-			const queue = new Queue([1, 2, 3])
-
-			expect(queue.dequeue()).toEqual(1)
-			expect(queue.elements).toEqual([2, 3])
-			expect(queue.dequeue()).toEqual(2)
-			expect(queue.elements).toEqual([3])
-			expect(queue.dequeue()).toEqual(3)
-			expect(queue.elements).toEqual([])
-			expect(queue.dequeue()).toEqual(undefined)
-			expect(queue.elements).toEqual([])
-		})
-	})
+            expect(queue.dequeue()).toEqual(1)
+            expect(queue.elements).toEqual([2, 3])
+            expect(queue.dequeue()).toEqual(2)
+            expect(queue.elements).toEqual([3])
+            expect(queue.dequeue()).toEqual(3)
+            expect(queue.elements).toEqual([])
+            expect(queue.dequeue()).toEqual(undefined)
+            expect(queue.elements).toEqual([])
+        })
+    })
 })

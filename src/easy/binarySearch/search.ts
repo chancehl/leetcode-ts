54776f26395764ffd50a56cae95d9577242f2176
Chance Linz <chanlinz@amazon.com>
2023-04-25 14:40:37 -0700

    ✨ feat(search.ts): add binary search algorithm implementation ✅ test(search.test.ts): add tests for binary search algorithm implementation

---
+++ src/easy/binarySearch/search.ts
@@ -0,0 +1,22 @@
+function search(nums: number[], target: number): number {
+	let slow = 0
+	let fast = nums.length - 1
+
+	while (slow <= fast) {
+		let mid = Math.floor((slow + fast) / 2)
+
+		if (nums[mid] === target) {
+			return mid
+		} else if (nums[mid] > target) {
+			// search left
+			fast = mid - 1
+		} else {
+			// search right
+			slow = mid + 1
+		}
+	}
+
+	return -1
+}
+
+export { search }

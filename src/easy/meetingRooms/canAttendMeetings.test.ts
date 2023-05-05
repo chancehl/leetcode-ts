3a598f5ae454d0b64ba23d45962c1e7d1fdeb4f2
Chance Linz <chanlinz@amazon.com>
2023-05-05 12:26:00 -0700

    🔥 refactor(canAttendMeetings.test.ts): remove unused hasOverlap function and tests

--- src/easy/meetingRooms/canAttendMeetings.test.ts
+++ src/easy/meetingRooms/canAttendMeetings.test.ts
@@ -1,4 +1,4 @@
-import { canAttendMeetings, hasOverlap } from './canAttendMeetings'
+import { canAttendMeetings } from './canAttendMeetings'
 
 describe('canAttendMeetings', () => {
     test('input 1', () => {
@@ -28,23 +28,5 @@ describe('canAttendMeetings', () => {
                 [90, 120],
             ]),
         ).toEqual(false)
-    })
-})
-
-describe('hasOverlap', () => {
-    test('input 1', () => {
-        expect(hasOverlap([0, 10], [5, 15])).toEqual(true)
-    })
-
-    test('input 2', () => {
-        expect(hasOverlap([5, 10], [2, 7])).toEqual(true)
-    })
-
-    test('input 3', () => {
-        expect(hasOverlap([0, 5], [10, 15])).toEqual(false)
-    })
-
-    test('input 4', () => {
-        expect(hasOverlap([13, 15], [1, 13])).toEqual(false)
     })
 })
